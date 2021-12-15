3ca955a0dfcdf37477a362112e988d7e2c96fda3
Vincent BACHELET <vbachele@e2r4p18.clusters.42paris.fr>
2021-12-15 16:15:34 +0100

    prout

--- src/utils/eating_and_death_philo.c
+++ src/utils/eating_and_death_philo.c
@@ -6,7 +6,7 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:10:42 by vbachele          #+#    #+#             */
-/*   Updated: 2021/12/14 19:40:00 by vbachele         ###   ########.fr       */
+/*   Updated: 2021/12/15 15:35:55 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 int	check_if_number_of_has_been_eaten(t_philo *philo)
 {
-	// int	i;
 	int j;
 
-	// i = 0;
 	j = 0;
 	if (philo->root->number_of_philosophers % 2 == 0)
 		j = philo->root->number_of_philosophers;
@@ -39,14 +37,13 @@ int	check_if_philo_is_dead(t_philo *philo)
 	int				time_since_last_lunch;
 	int				current_time;
 	
+	time_since_last_lunch = 0;
 	pthread_mutex_lock(&philo->root->death[philo->id]);
 	current_time = get_current_time(philo);
 	gettimeofday(&count, 0);
-	// gettimeofday(&philo->root->end, 0);
 	while (time_since_last_lunch <= philo->root->time_to_eat)
 	{
 		usleep(50);
-		gettimeofday(&philo->root->end, 0);
 		time_since_last_lunch = (philo->root->end.tv_sec * 1000
 		+ philo->root->end.tv_usec / 1000) \
 		- ( count.tv_sec * 1000 +  count.tv_usec / 1000);
@@ -55,6 +52,7 @@ int	check_if_philo_is_dead(t_philo *philo)
 			printf("%d ms: philo %d is DEAD\n", current_time, philo->id);
 			return (TRUE);
 		}
+		gettimeofday(&philo->root->end, 0);
 	}
 	pthread_mutex_unlock(&philo->root->death[philo->id]);
 	return (FALSE);
